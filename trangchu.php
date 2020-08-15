89605357a207a7f0a2724e6740682b6b559bd1ed
mrlgpro <69710392+mrlgpro@users.noreply.github.com>
2020-08-15 14:57:47 +0700

    Update trangchu.php

--- trangchu.php
+++ trangchu.php
@@ -7,9 +7,9 @@
 <html>
 <head>
 <title>Trang chủ</title>
-<script src="js/jquery.min.js"></script>
+<script src="jquery.min.js"></script>
 <!-- Custom Theme files -->
-<link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
+<link href="style.css" rel="stylesheet" type="text/css" media="all"/>
 <!-- for-mobile-apps -->
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
